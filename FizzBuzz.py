5601f6f341271da1f32b2d6a4ef95238e057c00f
TheCoderKitty <72262190+UstmanAbdurahman@users.noreply.github.com>
2021-01-20 10:41:48 +0700

    ♻️✏️Changed parameter names

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,7 +1,7 @@
 class `Algorithms`:
-    def FizzBuzz(par1,par2): #the fizz buzz function
-        num1 = par1
-        num2 = par2
+    def FizzBuzz(param1,param2): #the fizz buzz function
+        num1 = param1
+        num2 = param2
         for i in range(100):
             if i % num1 == 0 and i % num2 == 0: #this prints FizzBuzz IF "i" can be divided by num1 AND num2
                 print("FizzBuzz")
